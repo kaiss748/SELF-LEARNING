b6ae3dd19c61dc580251ec19908210b03201b170
kaiss748 <yubelkaiii@gmail.com>
2025-08-15 10:06:57 +0700

    selflearn

---
+++ tuple&set.py
@@ -0,0 +1,27 @@
+#LIST
+# Menggunakan kurung siku []
+data_list = [2, 6, 12, 29] 
+print(data_list)
+
+#TUPLES
+
+data_tuples = (7,8,9,10)
+print(data_tuples)
+print(data_tuples[1])
+
+
+#data_tuples[1] = 6
+#data_tuples.append(4)
+"""
+akan TypeError karena data didalam tuples tdk dapat
+dirubah, bgtu pula append
+"""
+
+#SETS (himpunan)
+data_sets = {1,2,3,4,5,7,8}
+print(data_sets)
+#print(data_sets[0])
+"""
+set adalah data himpunan, tdk ada urutan atau pun index
+jadi ga bisa dipanggil one by one, tapi bisa ditambahkan.
+"""
